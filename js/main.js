8b036b4589ab6c1451c8910f21eefeaddfd96b8d
pbhumika198 <122385882+pbhumika198@users.noreply.github.com>
2025-09-07 12:17:50 +0530

    Initial commit - added SkillBridge Academy  Traning Website project

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) currentSlide = 0;
+    if (index < 0) currentSlide = slides.length - 1;
+
+    const offset = -currentSlide * 100;
+    document.querySelector('.slider').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    currentSlide++;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide--;
+    showSlide(currentSlide);
+}
+
+// Initialize the slider
+showSlide(currentSlide);
